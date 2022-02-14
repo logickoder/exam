4442f4e910219333a53d06773fa58f30a5594948
Jeffrey Orazulike <chukwudumebiorazulike@gmail.com>
2022-02-14 06:39:25 +0100

    [feat] finished create new exam info fragment design

--- app/src/main/java/dev/logickoder/edvora/ui/main/exams/CreateExamFragment.kt
+++ app/src/main/java/dev/logickoder/edvora/ui/main/exams/CreateExamFragment.kt
@@ -5,7 +5,7 @@ import dev.logickoder.edvora.databinding.FragmentCreateExamBinding
 import dev.logickoder.edvora.ui.base.BaseFragment
 import dev.logickoder.edvora.utils.view.viewBinding
 
-class CreateExamFragment : BaseFragment(R.layout.fragment_exams) {
+class CreateExamFragment : BaseFragment(R.layout.fragment_create_exam) {
 
     override val binding by viewBinding(FragmentCreateExamBinding::bind)
 }
--- app/src/main/java/dev/logickoder/edvora/ui/main/exams/ExamsFragment.kt
+++ app/src/main/java/dev/logickoder/edvora/ui/main/exams/ExamsFragment.kt
@@ -1,6 +1,8 @@
 package dev.logickoder.edvora.ui.main.exams
 
-import androidx.viewbinding.ViewBinding
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import dev.logickoder.edvora.R
 import dev.logickoder.edvora.databinding.FragmentExamsBinding
 import dev.logickoder.edvora.ui.base.BaseFragment
@@ -9,4 +11,9 @@ import dev.logickoder.edvora.utils.view.viewBinding
 class ExamsFragment : BaseFragment(R.layout.fragment_exams) {
 
     override val binding by viewBinding(FragmentExamsBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        findNavController().navigate(R.id.action_examsFragment_to_createExamFragment)
+    }
 }
